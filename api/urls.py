dec2d4902f90dbb220aeda9b294ad1ba44a96df0
KarpovDenis74 <fotokarpov@yandex.ru>
2020-10-08 22:54:37 +0300

    pytest: OK, Att:3

--- api/urls.py
+++ api/urls.py
@@ -7,18 +7,18 @@ from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'v1/posts/(?P<post_id>\d+)/comments',
+router.register(r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet, basename='api_posts')
-router.register(r'v1/posts',
+router.register(r'posts',
     PostViewSet, basename='api_comments')
-router.register(r'v1/group',
+router.register(r'group',
     GroupViewSet, basename='api_groups')
-router.register(r'v1/follow',
+router.register(r'follow',
     FollowViewSet, basename='api_follow')
 
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
     ]
